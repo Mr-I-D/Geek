76faf1799f482ad8125db036618cf990282766d9
Mr-I-D <148846699+Mr-I-D@users.noreply.github.com>
2023-12-27 09:53:23 +0300

    Add files via upload

---
+++ 8/Home_Work/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите артикул стеклопакета:");
+        string input = Console.ReadLine();
+
+        GlassPackageInfo packageInfo = ParseGlassPackage(input);
+
+        if (packageInfo != null)
+        {
+            Console.WriteLine($"Камерность: {(packageInfo.ChamberCount == 1 ? "Однокамерный" : "Двухкамерный")}");
+            Console.WriteLine($"Толщина всего СП: {packageInfo.TotalThickness} мм");
+            Console.WriteLine($"Толщина стекла: {packageInfo.GlassThickness} мм");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный формат артикула стеклопакета.");
+        }
+    }
+
+    static GlassPackageInfo ParseGlassPackage(string input)
+    {
+        string pattern = @"\b(\d+)\s?(Стекло|Рамка)\b";
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        if (matches.Count > 0)
+        {
+            int chamberCount = matches.Count(x => x.Groups[2].Value == "Рамка");
+
+            int totalThickness = matches.Cast<Match>().Sum(m => int.Parse(m.Groups[1].Value));
+            int glassThickness = int.Parse(matches[0].Groups[1].Value);
+
+            return new GlassPackageInfo
+            {
+                ChamberCount = chamberCount,
+                TotalThickness = totalThickness,
+                GlassThickness = glassThickness
+            };
+        }
+
+        return null;
+    }
+}
+
+class GlassPackageInfo
+{
+    public int ChamberCount { get; set; }
+    public int TotalThickness { get; set; }
+    public int GlassThickness { get; set; }
+}
